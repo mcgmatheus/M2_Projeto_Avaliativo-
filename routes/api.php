02e80859b110707a5c3f592d6d46c0846fd98e23
mcgmatheus <matheus.mcg@hotmail.com.br>
2023-12-27 17:05:27 -0300

    Implementação parcial da dashboard
    Será concluída após realização do cadastro de exercícios e estudantes

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,13 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/dashboard/{id}', [DashboardController::class, 'index']);
 });
 
 // rota pública
