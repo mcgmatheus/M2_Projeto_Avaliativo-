38c5f83aef8a4c5e5ebb5726523df9cb1705126b
mcgmatheus <matheus.mcg@hotmail.com.br>
2023-12-28 15:28:41 -0300

    Feat: deleta usuario

--- routes/api.php
+++ routes/api.php
@@ -16,6 +16,7 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::delete('/exercises/{id}', [ExerciseController::class, 'destroy']);
     Route::post('/students', [StudentController::class, 'store'])->middleware(verifyUserAccountLimit::class);
     Route::get('/students', [StudentController::class, 'index']);
+    Route::delete('/students/{id}', [StudentController::class, 'destroy']);
 });
 
 // rota pública
