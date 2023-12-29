f45cd7ced5dc727386c7d593d140a5e693803296
mcgmatheus <matheus.mcg@hotmail.com.br>
2023-12-29 12:16:07 -0300

    Inicia exportação de pdf
    Cria controller e rota.
    Cria variáveis para armazenar os dados necessários

---
+++ app/Http/Controllers/StudentReportController.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Student;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class StudentReportController extends Controller
+{
+    public function reportWorkouts(Request $request)
+    {
+        //INSTALL DOMPDF
+        //composer require barryvdh/laravel-dompdf
+        try {
+            $userId = Auth::user()->id;
+            $studentId = $request->input('id');
+            $studentData = Student::with('workouts.exercise')->findOrFail($studentId);
+            if ($userId === $studentData->user_id) {
+                $studentName = $studentData->name;
+                $mondayWorkout = $studentData->workouts->where('day', 'SEGUNDA')->sortBy('created_at')->map(function ($workout) {
+                    return [
+                        'exercise_description' => $workout->exercise->description,
+                        'repetitions' => $workout->repetitions,
+                        'weight' => $workout->weight,
+                        'break_time' => $workout->break_time,
+                        'day' => $workout->day,
+                        'observations' => $workout->observations,
+                        'time' => $workout->time,
+                    ];
+                });
+                $tuesdayWorkout = $studentData->workouts->where('day', 'TERCA')->sortBy('created_at')->map(function ($workout) {
+                    return [
+                        'exercise_description' => $workout->exercise->description,
+                        'repetitions' => $workout->repetitions,
+                        'weight' => $workout->weight,
+                        'break_time' => $workout->break_time,
+                        'day' => $workout->day,
+                        'observations' => $workout->observations,
+                        'time' => $workout->time,
+                    ];
+                });
+                $WednesdayWorkout = $studentData->workouts->where('day', 'QUARTA')->sortBy('created_at')->map(function ($workout) {
+                    return [
+                        'exercise_description' => $workout->exercise->description,
+                        'repetitions' => $workout->repetitions,
+                        'weight' => $workout->weight,
+                        'break_time' => $workout->break_time,
+                        'day' => $workout->day,
+                        'observations' => $workout->observations,
+                        'time' => $workout->time,
+                    ];
+                });
+                $thursdayWorkout = $studentData->workouts->where('day', 'QUINTA')->sortBy('created_at')->map(function ($workout) {
+                    return [
+                        'exercise_description' => $workout->exercise->description,
+                        'repetitions' => $workout->repetitions,
+                        'weight' => $workout->weight,
+                        'break_time' => $workout->break_time,
+                        'day' => $workout->day,
+                        'observations' => $workout->observations,
+                        'time' => $workout->time,
+                    ];
+                });
+                $fridayWorkout = $studentData->workouts->where('day', 'SEXTA')->sortBy('created_at')->map(function ($workout) {
+                    return [
+                        'exercise_description' => $workout->exercise->description,
+                        'repetitions' => $workout->repetitions,
+                        'weight' => $workout->weight,
+                        'break_time' => $workout->break_time,
+                        'day' => $workout->day,
+                        'observations' => $workout->observations,
+                        'time' => $workout->time,
+                    ];
+                });
+                $saturdayWorkout = $studentData->workouts->where('day', 'SABADO')->sortBy('created_at')->map(function ($workout) {
+                    return [
+                        'exercise_description' => $workout->exercise->description,
+                        'repetitions' => $workout->repetitions,
+                        'weight' => $workout->weight,
+                        'break_time' => $workout->break_time,
+                        'day' => $workout->day,
+                        'observations' => $workout->observations,
+                        'time' => $workout->time,
+                    ];
+                });
+                $sundayWorkout = $studentData->workouts->where('day', 'DOMINGO')->sortBy('created_at')->map(function ($workout) {
+                    return [
+                        'exercise_description' => $workout->exercise->description,
+                        'repetitions' => $workout->repetitions,
+                        'weight' => $workout->weight,
+                        'break_time' => $workout->break_time,
+                        'day' => $workout->day,
+                        'observations' => $workout->observations,
+                        'time' => $workout->time,
+                    ];
+                });
+                return;
+            }
+            if ($studentData->user_id !== $userId) {
+                return response()->json(['message' => 'Acesso não autorizado'], 403);
+            }
+        } catch (ModelNotFoundException $exception) {
+            if (!$studentId) return response()->json(['message' => 'É necessário informar um id de aluno'], 400);
+            return response()->json(['message' => 'Aluno não encontrado'], 404);
+        } catch (\Exception $exception) {
+            return response()->json(['message' => $exception->getMessage()], 400);
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ExerciseController;
 use App\Http\Controllers\StudentController;
+use App\Http\Controllers\StudentReportController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\WorkoutController;
 use App\Http\Middleware\verifyUserAccountLimit;
@@ -11,6 +12,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->group(function () {
 
+    Route::get('/students/export/studentId', [StudentReportController::class, 'reportWorkouts']);
     Route::get('/students/{id}/workouts', [WorkoutController::class, 'show']);
     Route::post('/workouts', [WorkoutController::class, 'store']);
     Route::post('/logout', [AuthController::class, 'logout']);
